7b2241059166de91db6c6b973b00c29b6ec0fec5
shin donggyu <donggyu9410@gmail.com>
2019-09-13 13:16:49 +0900

    using interface in typescript

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+import { User } from './types/types';
 
 const app: Application = express();
 
@@ -6,6 +7,12 @@ const add = (a: number, b: number): number => a + b
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     console.log(add(5, 5));
+    const userJson: User = {
+        name: 'asd',
+        age: 123,
+        created: '123'
+    }
+
     res.send('Hello');
 })
 
